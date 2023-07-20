aaad45613ee9e8799db3fce13457c3ae34677568
1GodLess5 <gacekalbert@seznam.cz>
2023-07-20 20:58:11 +0200

    added display functionality -> display "_" for guessing

--- day-7-HANGMAN/main.py
+++ day-7-HANGMAN/main.py
@@ -1,6 +1,7 @@
 import random
 
 wordsList = []
+display = []
 
 wordsFile = open("wordsList.txt", "r")
 for word in wordsFile:
@@ -10,17 +11,47 @@ wordsFile.close()
 fileLength = len(wordsList)
 chosenWord = wordsList[random.randint(0, fileLength - 1)]
 
+for letter in chosenWord:
+    display.append("_")
+print(display)
+
 # cheating
 print(chosenWord)
 
 exitHangman = False
+wrongGuessCounter = 0
 
+# main loop
 while not exitHangman:
+    displayCounter = 0
     userGuess = input("Guess a letter:\t").lower()
 
     for letter in chosenWord:
         if userGuess == letter:
-            print("Yup")
+            display[displayCounter] = letter
         else:
             print("Nope")
+        displayCounter += 1
+
+    print(display)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
