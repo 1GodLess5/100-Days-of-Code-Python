028f447a92637106149b1016b730f2398b079528
1GodLess5 <gacekalbert@seznam.cz>
2023-07-20 21:46:04 +0200

    added design.py -> hangman stages ascii, user loose

--- day-7-HANGMAN/main.py
+++ day-7-HANGMAN/main.py
@@ -1,5 +1,6 @@
 import random
 import os
+import time
 import design
 
 wordsList = []
@@ -21,28 +22,50 @@ print(display)
 print(chosenWord)
 
 exitHangman = False
-wrongGuessCounter = 0
+
+lives = 6
 
 # main loop
 while not exitHangman:
     displayCounter = 0
+    wrongGuessCounter = 0
     userGuess = input("Guess a letter:\t").lower()
 
     for letter in chosenWord:
         if userGuess == letter:
             display[displayCounter] = letter
         else:
-            pass
+            wrongGuessCounter += 1
         displayCounter += 1
-
     print(display)
 
+    if wrongGuessCounter == len(chosenWord):
+        lives -= 1
+        print(f"Lives left: {lives}")
+        print(design.stages[lives])
+    print("\n\n")
+
     if ''.join(display) == chosenWord:
+        time.sleep(3)
         os.system("clear")
+
         print(design.color.BOLD + design.color.GREEN)
-        print("You guessed the correct word " + ''.join(display).upper() + " !")
+        print(f"You guessed the correct word: {chosenWord.upper()} !")
         print("YOU WIN!")
         print(design.color.END)
+
+        exitHangman = True
+
+    elif lives == 0:
+        time.sleep(5)
+        os.system("clear")
+
+        print(design.stages[0])
+        print(design.color.BOLD + design.color.RED)
+        print(f"The correct word was: {chosenWord.upper()} !")
+        print("YOU LOOSE!")
+        print(design.color.END)
+
         exitHangman = True
 
 
